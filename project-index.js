5d14cfea5ef38d71b13225eea6271e7e7f652fda
SENDITTOAJAY <ajayrnld@gmail.com>
2024-01-19 22:18:05 +0530

    Function to get date and time

--- project-index.js
+++ project-index.js
@@ -23,3 +23,36 @@ const temp = document.getElementById("temp"),
   hourlyBtn = document.querySelector(".hourly"),
   weekBtn = document.querySelector(".week"),
   weatherCards = document.querySelector("#weather-cards");
+  
+  //Initialising
+  let currentCity = "";
+let currentUnit = "c";
+let hourlyorWeek = "week";
+
+
+// function to get date and time
+function getDateTime() {
+  let now = new Date(),
+    hour = now.getHours(),
+    minute = now.getMinutes();
+
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    // 12 hours format
+    hour = hour % 12;
+    if (hour < 10) {
+      hour = "0" + hour;
+    }
+    if (minute < 10) {
+      minute = "0" + minute;
+    }
+    let dayString = days[now.getDay()];
+    return `${dayString}, ${hour}:${minute}`;
+  }
