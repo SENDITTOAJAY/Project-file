854b9d29cb6a8d214dd5f4fff650207ffbf40ea1
SENDITTOAJAY <ajayrnld@gmail.com>
2024-01-28 21:42:30 +0530

    Function to change wheather icons

--- project-index.js
+++ project-index.js
@@ -163,3 +163,19 @@ function updateForecast(data, unit, type) {
     day++;
   }
 }
+// function to change weather icons
+function getIcon(condition) {
+  if (condition === "partly-cloudy-day") {
+    return "https://i.ibb.co/PZQXH8V/27.png";
+  } else if (condition === "partly-cloudy-night") {
+    return "https://i.ibb.co/Kzkk59k/15.png";
+  } else if (condition === "rain") {
+    return "https://i.ibb.co/kBd2NTS/39.png";
+  } else if (condition === "clear-day") {
+    return "https://i.ibb.co/rb4rrJL/26.png";
+  } else if (condition === "clear-night") {
+    return "https://i.ibb.co/1nxNGHL/10.png";
+  } else {
+    return "https://i.ibb.co/rb4rrJL/26.png";
+  }
+}
